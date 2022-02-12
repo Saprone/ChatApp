d7865891be33435d440a32d608e8c77d485d17c3
Onrust,Bas B <b.onrust@student.fontys.nl>
2022-02-12 17:23:30 +0100

    Revertbacktoinitiatialcommit

--- ChatAppFrontend/src/pages/LoginPage.vue
+++ ChatAppFrontend/src/pages/LoginPage.vue
@@ -1,16 +1,6 @@
 <template>
   <div class="loginpage">
-    <div class="panel">
-      <form action="#" @submit.prevent="login">
-        <div class="section">
-          <label for="username">Username:</label>
-          <input required type="text" name="username"/>  
-        </div>
-        <div class="section">
-          <button type="login">Login</button>
-        </div>
-      </form>
-    </div>
+    <h2>Login Page</h2> 
   </div>
 </template> 
 
@@ -20,51 +10,13 @@ import Stomp from 'webstomp-client'
 
 export default {
   name: 'LoginPage',
-  data: () => {
-    return {
-      usernames: [],
-      username: ''  
-    }
-  },
   created() {
     this.socket = new SockJS("http://localhost:8080/sockjs");
     this.stompClient = Stomp.over(this.socket);
-
-    this.stompClient.connect({}, frame => {
-      this.stompClient.subscribe("/topic/user", payload => {
-        if(payload.body !== null) {
-          this.usernames.push(payload.body);
-
-          this.$router.push({name: "Chatroom", params: { data: this.usernames }});
-        }
-      });
-    });
-  },  
-  methods: {
-    login(action) {
-      const {username} = Object.fromEntries(new FormData(action.target));
-      this.username = username;
-
-      if(this.stompClient && this.username != '') {
-        this.stompClient.send("/app/user.input", JSON.stringify({ username: this.username }), {});
-      }
-    }
+    this.stompClient.connect();
   }
 }
 </script>
 
 <style scoped>
-.loginpage .panel {
-   margin-top: 300px;
-}
-.loginpage form {
-  display: block;
-  margin: auto;
-  border: 1px solid black;
-  width: 200px;
-  padding: 15px 5px;
-}
-.loginpage .section:not(:first-of-type), .loginpage input {
-  margin-top: 10px;
-}
 </style>

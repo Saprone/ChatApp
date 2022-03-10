c84f7e06e0d2d8eaaacfb6f9f72977e1b0094ac3
Bas <onrust.bas@gmail.com>
2022-03-10 09:57:14 +0100

    Added websocket comments

--- ChatAppFrontend/src/pages/LoginPage.vue
+++ ChatAppFrontend/src/pages/LoginPage.vue
@@ -27,14 +27,21 @@ export default {
     }
   },
   created() {
+    //Create SockJs instance.
     this.socket = new SockJS("http://localhost:8080/sockjs");
     this.stompClient = Stomp.over(this.socket);
 
+    //Conncect to stomp Client.
     this.stompClient.connect({}, frame => {
+
+      //Subscribe to a STOMP Broker location.
       this.stompClient.subscribe("/topic/user", payload => {
         if(payload.body !== null) {
+
+          //Add usernames to payload. 
           this.usernames.push(payload.body);
 
+          //Navigate to route.
           this.$router.push({name: "Chatroom", params: { data: this.usernames }}).catch(()=>{});
         }
       });
@@ -46,6 +53,8 @@ export default {
       this.username = username;
 
       if(this.stompClient && this.username != '') {
+
+        //Send a message to a named destination/server.
         this.stompClient.send("/app/user.input", JSON.stringify({ username: this.username }), {});
       }
     }

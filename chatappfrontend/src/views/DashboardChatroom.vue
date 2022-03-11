d24aaeb724cca751a0d83363fabf8fbfd024a6f9
Onrust,Bas B <b.onrust@student.fontys.nl>
2022-03-11 13:09:41 +0100

    Added missing views

---
+++ chatappfrontend/src/views/DashboardChatroom.vue
@@ -0,0 +1,14 @@
+<template> 
+  <div class="dashboardchatroompage">
+    <h2>Dashboard Chatroom Page</h2>  
+  </div>
+</template> 
+
+<script>
+export default {
+  name: 'DashboardChatroomPage'
+}
+</script>
+
+<style scoped>
+</style>
---
+++ chatappfrontend/src/views/DashboardUsers.vue
@@ -0,0 +1,14 @@
+<template>  
+  <div class="dashboarduserspage">
+    <h2>Dashboard Users Page</h2>  
+  </div>
+</template> 
+
+<script>
+export default {
+  name: 'DashboardUsersPage'
+}
+</script>
+
+<style scoped>
+</style>

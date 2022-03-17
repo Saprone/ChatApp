e72aaa2c879e3b3d5b7bb79d7ea1016daae80d1a
Bas <onrust.bas@gmail.com>
2022-03-17 13:25:58 +0100

    Update WebsocketConnection.js

--- chatappfrontend/src/services/WebsocketConnection.js
+++ chatappfrontend/src/services/WebsocketConnection.js
@@ -8,18 +8,17 @@ export default {
         this.stompClient = Stomp.over(this.socket);
 
         //Conncect to stomp Client.
-        this.stompClient.connect({}, frame => {});
-    },
-    subscribeToStompBroker() {
-        //Subscribe to a STOMP Broker location.
-        this.stompClient.subscribe("/topic/user", payload => {
-            if(payload.body !== null) {
-                //Add usernames to payload. 
-                this.usernames.push(payload.body);
+        this.stompClient.connect({}, frame => {
+            //Subscribe to a STOMP Broker location.
+            this.stompClient.subscribe("/topic/user", payload => {
+                if(payload.body !== null) {
+                    //Add usernames to payload. 
+                    this.usernames.push(payload.body);
 
-                //Navigate to route.
-                this.$router.push({name: "Chatroom", params: { data: this.usernames }}).catch(()=>{});
-            }
+                    //Navigate to route.
+                    this.$router.push({name: "Chatroom", params: { data: this.usernames }}).catch(()=>{});
+                }
+            });
         });
     },
     sendMessageToServer() {

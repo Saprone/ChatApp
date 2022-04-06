4c67145090bdd148fb220ee1a3e6a0350d95a565
Onrust,Bas B <b.onrust@student.fontys.nl>
2022-04-06 13:42:02 +0200

    Added UserRepository and RoleRepository

---
+++ chatappbackend/src/main/java/edu/fontys/chatapp/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package edu.fontys.chatapp.repository;
+
+import edu.fontys.chatapp.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+    User findByUsername(String username);
+}

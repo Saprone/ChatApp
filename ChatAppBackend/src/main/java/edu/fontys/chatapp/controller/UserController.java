ec844f1130967c79a35995fb3d404225332b39ca
Onrust,Bas B <b.onrust@student.fontys.nl>
2022-04-06 15:06:05 +0200

    Update app props and added mapping getusers()

--- ChatAppBackend/src/main/java/edu/fontys/chatapp/controller/UserController.java
+++ ChatAppBackend/src/main/java/edu/fontys/chatapp/controller/UserController.java
@@ -1,18 +1,28 @@
 package edu.fontys.chatapp.controller;
 
 import edu.fontys.chatapp.model.User;
+import edu.fontys.chatapp.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import java.util.List;
 
-@RestController
-@Tag(name = "User")
+@RestController @RequestMapping("/api") @RequiredArgsConstructor @Tag(name = "User")
 public class UserController {
+    private final UserService userService;
 
     @MessageMapping("/user.input")
     @SendTo("/topic/user")
-    public String UserModel(User userModel) throws Exception {
+    public String UserModel(User userModel) {
         return userModel.getUsername();
+    }
+
+    @GetMapping("/users") @Operation(summary = "Fetching all users")
+    public ResponseEntity<List<User>>getUsers() {
+        return ResponseEntity.ok().body(userService.getUsers());
     }
 }

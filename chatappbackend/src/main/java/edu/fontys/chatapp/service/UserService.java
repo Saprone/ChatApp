c0bc40e7611684187869a25aa724a217a9257435
Onrust,Bas B <b.onrust@student.fontys.nl>
2022-04-06 14:00:29 +0200

    Added UserService and UserServiceImplementation

---
+++ chatappbackend/src/main/java/edu/fontys/chatapp/service/UserService.java
@@ -0,0 +1,13 @@
+package edu.fontys.chatapp.service;
+
+import edu.fontys.chatapp.model.Role;
+import edu.fontys.chatapp.model.User;
+import java.util.List;
+
+public interface UserService {
+    User saveUser(User user);
+    Role saveRole(Role role);
+    void addRoleToUser(String username, String roleName);
+    User getUser(String username);
+    List<User> getUsers();
+}

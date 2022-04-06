0c84923dd004dca0964efffe2a2e7fd15115081f
Onrust,Bas B <b.onrust@student.fontys.nl>
2022-04-06 12:13:25 +0200

    Update models and install lombok

--- chatappbackend/src/main/java/edu/fontys/chatapp/model/Role.java
+++ chatappbackend/src/main/java/edu/fontys/chatapp/model/Role.java
@@ -4,19 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.UUID;
 
 @Entity @Data @NoArgsConstructor @AllArgsConstructor
-public class User {
+public class Role {
     @Id @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
     private String name;
-    private String username;
-    private String password;
-    private String room;
-    @ManyToMany(fetch = FetchType.EAGER)
-    private Collection<Role> roles = new ArrayList<>();
-    private Collection<String> chatMessages = new ArrayList<>();
 }

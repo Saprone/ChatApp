7e30dd4ba775e94a54c2c221acea993d72fab4fa
Onrust,Bas B <b.onrust@student.fontys.nl>
2022-04-07 11:31:16 +0200

    Removed commented out returns

--- chatappbackend/src/main/java/edu/fontys/chatapp/service/UserServiceImplementation.java
+++ chatappbackend/src/main/java/edu/fontys/chatapp/service/UserServiceImplementation.java
@@ -18,30 +18,27 @@ public class UserServiceImplementation implements UserService {
     @Override
     public User saveUser(User user) {
         log.info("Saving new user {} to the database", user.getName());
-        //return userRepository.save(user);
-        return null;
+        return userRepository.save(user);
     }
 
     @Override
     public Role saveRole(Role role) {
         log.info("Saving new role {} to the database", role.getName());
-        //return roleRepository.save(role);
-        return null;
+        return roleRepository.save(role);
     }
 
     @Override
     public void addRoleToUser(String username, String roleName) {
         log.info("Adding role {} to user {}", roleName, username);
-        //User user = userRepository.findByUsername(username);
-        //Role role = roleRepository.findByName(roleName);
-        //user.getRoles().add(role);
+        User user = userRepository.findByUsername(username);
+        Role role = roleRepository.findByName(roleName);
+        user.getRoles().add(role);
     }
 
     @Override
     public User getUser(String username) {
         log.info("Fetching user {}", username);
-        //return userRepository.findByUsername(username);
-        return null;
+        return userRepository.findByUsername(username);
     }
 
     @Override

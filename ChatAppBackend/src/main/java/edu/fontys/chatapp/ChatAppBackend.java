f83ca63f21bffadca444875ce290bec316816079
Onrust,Bas B <b.onrust@student.fontys.nl>
2022-04-07 17:12:53 +0200

    Added JWT Authentication and Authorization

--- ChatAppBackend/src/main/java/edu/fontys/chatapp/ChatAppBackend.java
+++ ChatAppBackend/src/main/java/edu/fontys/chatapp/ChatAppBackend.java
@@ -9,6 +9,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 import java.util.ArrayList;
 
 @SpringBootApplication
@@ -39,6 +42,11 @@ public class ChatAppBackend {
 			userService.addRoleToUser("kees", "ROLE_MANAGER");
 			userService.addRoleToUser("hans", "ROLE_USER");
 		};
+	}
+
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 	@Bean

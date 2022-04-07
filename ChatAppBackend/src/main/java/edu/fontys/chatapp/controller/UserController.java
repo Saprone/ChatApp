cf795326c9c606c2c17c361b2b8db345f26fc593
Onrust,Bas B <b.onrust@student.fontys.nl>
2022-04-07 11:47:09 +0200

    Added saveUser() in UserController

--- ChatAppBackend/src/main/java/edu/fontys/chatapp/controller/UserController.java
+++ ChatAppBackend/src/main/java/edu/fontys/chatapp/controller/UserController.java
@@ -7,6 +7,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import java.net.URI;
 import java.util.List;
 
 @RestController @RequestMapping("/api") @RequiredArgsConstructor
@@ -24,5 +26,9 @@ public class UserController {
         return ResponseEntity.ok().body(userService.getUsers());
     }
 
-
+    @PostMapping("/user/save")
+    public ResponseEntity<User>saveUser(@RequestBody User user) {
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/user/save").toUriString());
+        return ResponseEntity.created(uri).body(userService.saveUser(user));
+    }
 }

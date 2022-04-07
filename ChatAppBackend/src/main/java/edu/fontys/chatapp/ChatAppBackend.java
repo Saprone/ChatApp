61d698e1e5739081ffdc197a2b38034f6fc050c1
Onrust,Bas B <b.onrust@student.fontys.nl>
2022-04-07 14:57:16 +0200

    Added users to database

--- ChatAppBackend/src/main/java/edu/fontys/chatapp/ChatAppBackend.java
+++ ChatAppBackend/src/main/java/edu/fontys/chatapp/ChatAppBackend.java
@@ -1,10 +1,15 @@
 package edu.fontys.chatapp;
 
+import edu.fontys.chatapp.model.Role;
+import edu.fontys.chatapp.model.User;
+import edu.fontys.chatapp.service.UserService;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class ChatAppBackend {
@@ -15,6 +20,25 @@ public class ChatAppBackend {
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
+	}
+
+	@Bean
+	CommandLineRunner run(UserService userService) {
+		return args -> {
+			userService.saveRole(new Role(null, "ROLE_USER"));
+			userService.saveRole(new Role(null, "ROLE_MANAGER"));
+			userService.saveRole(new Role(null, "ROLE_ADMIN"));
+			userService.saveRole(new Role(null, "ROLE_SUPER_ADMIN"));
+
+			userService.saveUser(new User(null, "Piet P", "piet", "1234", "room1", new ArrayList<>(), new ArrayList<>()));
+			userService.saveUser(new User(null, "Kees K", "kees", "1234", "room2", new ArrayList<>(), new ArrayList<>()));
+			userService.saveUser(new User(null, "Hans H", "hans", "1234", "room1", new ArrayList<>(), new ArrayList<>()));
+
+			//userService.addRoleToUser("piet", "ROLE_SUPER_ADMIN");
+			//userService.addRoleToUser("piet", "ROLE_ADMIN");
+			//userService.addRoleToUser("kees", "ROLE_MANAGER");
+			//userService.addRoleToUser("hans", "ROLE_USER");
+		};
 	}
 
 	@Bean

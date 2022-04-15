d46989f962753dd65a3bd4bc4d5494bfefc51888
Onrust,Bas B <b.onrust@student.fontys.nl>
2022-04-15 12:24:12 +0200

    Update authority permitments

--- chatappbackend/src/main/java/edu/fontys/chatapp/security/SecurityConfiguration.java
+++ chatappbackend/src/main/java/edu/fontys/chatapp/security/SecurityConfiguration.java
@@ -5,19 +5,14 @@ import edu.fontys.chatapp.filter.CustomAuthorizationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.server.SecurityWebFilterChain;
-
 import static org.springframework.http.HttpMethod.GET;
 import static org.springframework.http.HttpMethod.POST;
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
@@ -37,12 +32,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManagerBean());
         customAuthenticationFilter.setFilterProcessesUrl("/api/login");
 
-        http.cors().and().csrf().disable();
-
+        http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(STATELESS);
-
         http.authorizeHttpRequests()
-            .antMatchers("**").permitAll()
+            .antMatchers("/api/login/**", "/api/test").permitAll()
+            .antMatchers(GET, "/api/users").hasAnyAuthority("ROLE_ADMIN", "ROLE_USER")
+            .antMatchers(POST, "/api/user/save/**").hasAnyAuthority("ROLE_ADMIN")
             .anyRequest().authenticated();
 
         http.addFilter(customAuthenticationFilter);

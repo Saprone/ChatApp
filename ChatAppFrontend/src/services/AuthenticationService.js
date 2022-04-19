0ea4198b702452e7c873a1c2659e86cdc917903d
Onrust,Bas B <b.onrust@student.fontys.nl>
2022-04-19 20:10:03 +0200

    Added async to authenticateUser()

--- ChatAppFrontend/src/services/AuthenticationService.js
+++ ChatAppFrontend/src/services/AuthenticationService.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const API_URL = "http://localhost:8082/api/login";
 
 export default new class AuthenticationService {
-    authenticateUser(username, password) {   
+    async authenticateUser(username, password) {   
         const params = new URLSearchParams();
         params.append('username', username);
         params.append('password', password);
@@ -12,8 +12,8 @@ export default new class AuthenticationService {
             headers: {
                 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
             }
-        }    
+        };    
         
-        return axios.post(API_URL, params, config)
-    }
-}
+        return await axios.post(API_URL, params, config);
+    };
+};

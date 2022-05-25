8de00f95a14a9dc520df551599b8c6ee27807b05
Onrust,Bas B <b.onrust@student.fontys.nl>
2022-05-25 13:15:09 +0200

    Mock axios setup

--- ChatAppFrontend/tests/unit/authenticationService.spec.js
+++ ChatAppFrontend/tests/unit/authenticationService.spec.js
@@ -1,7 +1,5 @@
 import AuthenticationService from '@/services/AuthenticationService.js'
 
-jest.mock("axios");
-
 describe('AuthenticationService.js', () => {
   it('test if authenticateUser in AuthenticationService is a function', () => {
     expect(typeof AuthenticationService.authenticateUser).toBe('function')
--- ChatAppFrontend/tests/unit/userService.spec.js
+++ ChatAppFrontend/tests/unit/userService.spec.js
@@ -1,17 +1,23 @@
 import UserService from '@/services/UserService.js'
 
+/*jest.mock("axios");
+mockAxios.get.mockImplementation(() => Promise.resolve({
+
+}));*/
+
 describe('UserService.js', () => {
   it('test if getUsers in UserService is a function', () => {
-    expect(typeof UserService.getUsers).toBe('function')
+    expect(typeof UserService.getUsers).toBe('function');
   })
 
   it('should return admin username', async () => {
     const response = await UserService.getUsers();
-    expect(response[0].username).toBe("admin")
+    console.log(response);
+    expect(response[0].username).toBe("admin");
   });
 
   it('should return user username', async () => {
     const response = await UserService.getUsers();
-    expect(response[1].username).toBe("user")
+    expect(response[1].username).toBe("user");
   });
 })  

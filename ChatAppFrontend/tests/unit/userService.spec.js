92bc06327bac476afea274f949d3828ae522a7b2
Onrust,Bas B <b.onrust@student.fontys.nl>
2022-05-25 13:39:53 +0200

    Got async mocked getUsers working

--- ChatAppFrontend/tests/unit/userService.spec.js
+++ ChatAppFrontend/tests/unit/userService.spec.js
@@ -1,23 +1,26 @@
 import UserService from '@/services/UserService.js'
+import mockAxios from 'axios'
 
-/*jest.mock("axios");
-mockAxios.get.mockImplementation(() => Promise.resolve({
-
-}));*/
+jest.mock("axios")
+const data = [{ username: "admin"}, { username: "user"}]
+mockAxios.get.mockResolvedValue({data})
 
 describe('UserService.js', () => {
+  afterEach(jest.clearAllMocks)
+
   it('test if getUsers in UserService is a function', () => {
-    expect(typeof UserService.getUsers).toBe('function');
+    expect(typeof UserService.getUsers).toBe('function')
   })
 
   it('should return admin username', async () => {
-    const response = await UserService.getUsers();
-    console.log(response);
-    expect(response[0].username).toBe("admin");
-  });
+    const response = await UserService.getUsers()
+    expect(response[0].username).toBe("admin")
+    expect(mockAxios.get).toHaveBeenCalledTimes(1)
+  })
 
   it('should return user username', async () => {
-    const response = await UserService.getUsers();
-    expect(response[1].username).toBe("user");
-  });
+    const response = await UserService.getUsers()
+    expect(response[1].username).toBe("user")
+    expect(mockAxios.get).toHaveBeenCalledTimes(1)
+  })
 })  

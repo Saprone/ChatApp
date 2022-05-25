69f4773adee4dff7315a2a25274a5f17823ad510
Onrust,Bas B <b.onrust@student.fontys.nl>
2022-05-25 10:41:10 +0200

    Removed commented out test

--- ChatAppFrontend/tests/unit/authenticationService.spec.js
+++ ChatAppFrontend/tests/unit/authenticationService.spec.js
@@ -1,27 +1,3 @@
-/*import { mount, flushPromises } from '@vue/test-utils'
-import axios from 'axios'
-import User from "../models/user"
-
-jest.mock("axios", () => ({
-    post: () => Promise.resolve(new User('admin', '1234'))
-}));
-
-it('test login with username and password input', async () => {
-  const wrapper = mount(AuthenticationService)   
-
-  //await
-
-  // Let's assert that we've called axios.get the right amount of times and
-  // with the right parameters.
-  expect(axios.post).toHaveBeenCalledTimes(1)
-  expect(axios.post).toHaveBeenCalledWith('http://localhost:8082/api/login')
-
-  // Wait until the DOM updates.
-  await flushPromises()
-
-  //expect()
-})*/
-
 import AuthenticationService from '@/services/AuthenticationService.js'
 
 describe('AuthenticationService.js', () => {
@@ -29,3 +5,5 @@ describe('AuthenticationService.js', () => {
     expect(typeof AuthenticationService.authenticateUser).toBe('function')
   })
 })  
+
+//npm run test:unit

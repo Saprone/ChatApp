dc3d5089bb8d6ea7592e7e55f5f64326fb01a7b7
Onrust,Bas B <b.onrust@student.fontys.nl>
2022-05-25 10:11:05 +0200

    Fix outdated find function

--- ChatAppFrontend/tests/unit/authenticationService.spec.js
+++ ChatAppFrontend/tests/unit/authenticationService.spec.js
@@ -1,6 +1,5 @@
 /*import { mount, flushPromises } from '@vue/test-utils'
 import axios from 'axios'
-import AuthenticationService from '@/services/AuthenticationService.js'
 import User from "../models/user"
 
 jest.mock("axios", () => ({
@@ -22,3 +21,11 @@ it('test login with username and password input', async () => {
 
   //expect()
 })*/
+
+import AuthenticationService from '@/services/AuthenticationService.js'
+
+describe('AuthenticationService.js', () => {
+  it('test if authenticateUser in AuthenticationService is a function', () => {
+    expect(typeof AuthenticationService.authenticateUser).toBe('function')
+  })
+})  
--- ChatAppFrontend/tests/unit/login.spec.js
+++ ChatAppFrontend/tests/unit/login.spec.js
@@ -4,7 +4,7 @@ import Login from '@/views/Login.vue'
 describe('Login.vue', () => {
   it('test username input', () => {
     const wrapper = mount(Login)
-    const input_username = wrapper.find({ref: 'username'})
+    const input_username = wrapper.findComponent({ref: 'username'})
     input_username.setValue('admin')
 
     expect(input_username.element.value).toBe('admin')
@@ -13,7 +13,7 @@ describe('Login.vue', () => {
 describe('Login.vue', () => {
   it('test password input', () => {
     const wrapper = mount(Login)
-    const input_password = wrapper.find({ref: 'password'})
+    const input_password = wrapper.findComponent({ref: 'password'})
     input_password.setValue('1234')
 
     expect(input_password.element.value).toBe('1234')

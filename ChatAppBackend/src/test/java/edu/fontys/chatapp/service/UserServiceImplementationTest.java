13bd09142fcf944c9f76d129c76cd9054567d797
Bas <onrust.bas@gmail.com>
2022-06-02 11:54:05 +0200

    Added saveUserTest() unit test

--- ChatAppBackend/src/test/java/edu/fontys/chatapp/service/UserServiceImplementationTest.java
+++ ChatAppBackend/src/test/java/edu/fontys/chatapp/service/UserServiceImplementationTest.java
@@ -1,11 +1,15 @@
 package edu.fontys.chatapp.service;
 
+import edu.fontys.chatapp.model.User;
 import edu.fontys.chatapp.repository.UserRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import java.util.ArrayList;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.Mockito.verify;
 
 public class UserServiceImplementationTest {
@@ -27,7 +31,30 @@ public class UserServiceImplementationTest {
     void getUsersTest() {
         //when
         userRepository.findAll();
+
         //then
         verify(userRepository).findAll();
+    }
+
+    @Test
+    void saveUserTest() {
+        //given
+        User user = new User(
+                null,
+                "admin",
+                "1234",
+                "room1",
+                new ArrayList<>(),
+                new ArrayList<>()
+        );
+
+        //when
+        userRepository.save(user);
+
+        //then
+        ArgumentCaptor<User> userArgumentCaptor = ArgumentCaptor.forClass(User.class);
+        verify(userRepository).save(userArgumentCaptor.capture());
+        User capturedUser = userArgumentCaptor.getValue();
+        assertThat(capturedUser).isEqualTo(user);
     }
 }

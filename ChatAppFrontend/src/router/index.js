98d2a63b6410bb8c2acf844cfbc2918024eb8d81
Onrust,Bas B <b.onrust@student.fontys.nl>
2022-06-16 14:07:07 +0200

    update babel.config

--- ChatAppFrontend/src/router/index.js
+++ ChatAppFrontend/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Chatroom from '@/views/Chatroom'
+import Chatroom from '../views/Chatroom.vue'
 /*import Dashboardchatroom from './views/DashboardChatroom.vue'
 import Dashboardusers from './views/DashboardUsers.vue'
 import Login from './views/Login.vue'

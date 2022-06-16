4c0c3bdc31b4aaa9d84994b3762bc87973a56488
Onrust,Bas B <b.onrust@student.fontys.nl>
2022-06-16 12:57:12 +0200

    Update jest.config.js

--- ChatAppFrontend/jest.config.js
+++ ChatAppFrontend/jest.config.js
@@ -6,23 +6,10 @@ module.exports = {
     'json',
     'vue',
   ],
-  transform: {
-    '^.+\\.vue$': 'vue-jest',
-    '.+\\.(js|jsx|ts|tsx|css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$':
-      'jest-transform-stub',
-    '^.+\\.jsx?$': 'babel-jest',
-  },
   transformIgnorePatterns: [
     '<rootDir>/(node_modules)/',
   ],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
-  snapshotSerializers: [
-    'jest-serializer-vue',
-  ],
-  testMatch: [
-    '**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)',
-  ],
-  testURL: 'http://localhost/',
 }
